23cfab3dcd4a4a99da9900ef946bb434711c607b
HenrikFalla <henrik.falla@gmail.com>
2025-02-16 01:59:39 +0100

    Updated front page rendering and styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import LogoSvg, { LogoSubtitleSvg } from './UI/SVGs/logo';
 
 export default function Home() {
 	return (
-		<main className='m-auto p-5 flex flex-col items-center justify-center w-full max-w-6xl gap-4'>
-			<LogoSvg />
-			<LogoSubtitleSvg />
-		</main>
+		<div className='m-auto flex flex-col items-center justify-center w-[calc(100vw-36px)] h-[calc(100vh-36px)] rounded-2xl solid border-2 border-slate-50'>
+			<main className='m-auto p-5 flex flex-col items-center justify-center w-full max-w-6xl gap-4'>
+				<LogoSvg />
+				<LogoSubtitleSvg />
+			</main>
+		</div>
 	);
 }
-
