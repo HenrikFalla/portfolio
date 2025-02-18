cb407839874cdcec9173013d307fc9cebba0bd12
HenrikFalla <henrik.falla@gmail.com>
2025-02-18 19:23:12 +0100

    Updated nav for frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import { ResumeIcon } from './UI/SVGs/icons';
 import LogoSvg, { LogoSubtitleSvg } from './UI/SVGs/logo';
 // import { getUsers } from './api/(neon)/actions/route';
 
@@ -8,11 +10,25 @@ export default function Home() {
 	// 	console.log(u.email);
 	// }
 	return (
-		<div className='m-auto flex flex-col items-center justify-center w-[calc(100vw-36px)] h-[calc(100vh-36px)] rounded-2xl solid border-2 border-slate-950 dark:border-slate-50'>
-			<main className='m-auto p-5 flex flex-col items-center justify-center w-full max-w-6xl gap-4'>
-				<LogoSvg />
-				<LogoSubtitleSvg />
-			</main>
+		<div className='flex flex-col items-center justify-center w-screen h-screen p-8'>
+			<div className='w-full h-full rounded-2xl solid border-2 border-slate-950 dark:border-slate-50 p-8 flex flex-col items-center justify-center'>
+				<main className='m-auto p-5 flex flex-col items-center justify-center w-full max-w-6xl gap-4 relative'>
+					<nav className='w-full flex justify-end'>
+						<Link
+							href='/resume'
+							className='[&>h5]:hover:-rotate-6 [&>svg]:hover:rotate-6'
+						>
+							<h5 className='transform -rotate-12  ease-in-out duration-300'>
+								CV
+							</h5>
+							<ResumeIcon />
+						</Link>
+					</nav>
+
+					<LogoSvg />
+					<LogoSubtitleSvg />
+				</main>
+			</div>
 		</div>
 	);
 }
