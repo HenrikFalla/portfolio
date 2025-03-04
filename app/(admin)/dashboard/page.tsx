1c82d5c7d1a710537a217e3c50352ba0bf744dd9
HenrikFalla <henrik.falla@gmail.com>
2025-03-04 22:04:15 +0100

    Update to dashboard

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,45 +1,19 @@
+import Header from '@/app/(components)/nav/header';
 import Link from 'next/link';
 
 export default function Dashbaord() {
 	return (
-		<main className='flex flex-col items-center justify-center w-full h-full p-4 max-w-2xl'>
-			<h1>Dashboard</h1>
-			<section className='grid grid-cols-4 gap-4 m-4 w-full'>
-				<Link href='/dashboard/resume'>
-					<div className='flex flex-col items-center justify-center min-h-16 w-full rounded-2xl bg-foreground dark:bg-foreground-dark text-background dark:text-background-dark'>
-						<p>Resume Builder</p>
-					</div>
-				</Link>
-				<Link href='/dashboard/createCourse'>
-					<div className='flex flex-col items-center justify-center min-h-16 w-full rounded-2xl bg-foreground dark:bg-foreground-dark text-background dark:text-background-dark'>
-						<p>New Course</p>
-					</div>
-				</Link>
-				<Link href='/dashboard/resume'>
-					<div className='flex flex-col items-center justify-center min-h-16 w-full rounded-2xl bg-foreground dark:bg-foreground-dark text-background dark:text-background-dark'>
-						<p>Resume</p>
-					</div>
-				</Link>
-				<Link href='/dashboard/resume'>
-					<div className='flex flex-col items-center justify-center min-h-16 w-full rounded-2xl bg-foreground dark:bg-foreground-dark text-background dark:text-background-dark'>
-						<p>Resume</p>
-					</div>
-				</Link>
-				<Link href='/dashboard/resume'>
-					<div className='flex flex-col items-center justify-center min-h-16 w-full rounded-2xl bg-foreground dark:bg-foreground-dark text-background dark:text-background-dark'>
-						<p>Resume</p>
-					</div>
-				</Link>
-				<Link href='/dashboard/resume'>
-					<div className='flex flex-col items-center justify-center min-h-16 w-full rounded-2xl bg-foreground dark:bg-foreground-dark text-background dark:text-background-dark'>
-						<p>Resume</p>
-					</div>
-				</Link>
-				<Link href='/dashboard/resume'>
-					<div className='flex flex-col items-center justify-center min-h-16 w-full rounded-2xl bg-foreground dark:bg-foreground-dark text-background dark:text-background-dark'>
-						<p>Resume</p>
-					</div>
-				</Link>
+		<main className='grid grid-cols-12 gap-4 w-full h-full'>
+			<nav className='col-span-2 flex flex-col gap-4 p-8 bg-foreground dark:bg-foreground-dark text-background dark:text-background-dark h-full min-h-[100vh]'>
+				<Link href='/dashboard/resume'>Create reume item</Link>
+				<Link href='/dashboard/createCourse'>Create Course</Link>
+				<Link href='/dashboard/courses'>Edit Courses</Link>
+				<Link href='/dashboard/createProject'>Create Project</Link>
+				<Link href='/dashboard/projects'>Edit Projects</Link>
+			</nav>
+			<section className='flex flex-col col-span-10 items-center h-full justify-center'>
+				<h1>Dashboard</h1>
+				<Header />
 			</section>
 		</main>
 	);
